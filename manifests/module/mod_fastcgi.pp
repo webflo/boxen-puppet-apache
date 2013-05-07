69817ac4acb74010f0367107602f194696fb062f
Florian Weber <f.weber@digi-info.de>
2013-05-07 11:00:47 +0200

    Fixed order of config files.

--- manifests/module/mod_fastcgi.pp
+++ manifests/module/mod_fastcgi.pp
@@ -1,13 +1,13 @@
 class apache::module::mod_fastcgi {
   include apache::module::requirements
-  
+
   package { 'homebrew/apache/mod_fastcgi':
     ensure  => present,
     require => Class['Apache::module::requirements'],
   }
 
   $version = '2.4.6'
-  file { '/etc/apache2/other/mod_fastcgi.conf':
+  file { '/etc/apache2/other/00-mod_fastcgi.conf':
     ensure  => present,
     content => template('apache/config/apache/modules/mod_fastcgi.conf.erb'),
   }

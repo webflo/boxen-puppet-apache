3a5c89f294749ac39e39d195ac894062ed9aca0d
Florian Weber <f.weber@digi-info.de>
2013-05-27 16:33:28 +0200

    Ensure symlink.

--- manifests/module/requirements.pp
+++ manifests/module/requirements.pp
@@ -4,7 +4,7 @@ class apache::module::requirements {
   # https://github.com/Homebrew/homebrew-apache#troubleshooting
   file { "/Applications/Xcode.app/Contents/Developer/Toolchains/OSX${::macosx_productversion_major}.xctoolchain":
     ensure => 'link',
-    source => '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain',
+    target => '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain',
     owner => 'root',
     group => 'wheel'
   }

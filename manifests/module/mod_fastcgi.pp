a8004872a3c2a87914a715b21e25c49605247c1a
Florian Weber <florian.weber@ueberbit.de>
2014-07-28 09:48:34 +0200

    Compile mod_fastcgi wiht httpd 2.4

--- manifests/module/mod_fastcgi.pp
+++ manifests/module/mod_fastcgi.pp
@@ -1,12 +1,20 @@
 class apache::module::mod_fastcgi {
   include apache::module::requirements
 
-  package { 'homebrew/apache/mod_fastcgi':
-    ensure  => present,
-    require => Class['Apache::module::requirements'],
+  $version = '2.4.6-boxen1'
+
+  homebrew::formula { 'mod_fastcgi':
+    before => Package['boxen/brews/mod_fastcgi'],
   }
 
-  $version = '2.4.6'
+  package { 'boxen/brews/mod_fastcgi':
+    ensure => $version,
+    require => Class['Apache::module::requirements'],
+    install_options => [
+      '--with-osx-httpd24'
+    ]
+  }
+
   file { '/etc/apache2/other/00-mod_fastcgi.conf':
     ensure  => present,
     content => template('apache/config/apache/modules/mod_fastcgi.conf.erb'),

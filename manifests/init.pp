4c77ff023927d9e3673bef7ec7f87150e179ace3
Florian Weber <florian.weber@ueberbit.de>
2015-09-29 22:36:07 +0200

    Remove service definition

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,18 +2,8 @@ class apache {
   require apache::config
   require homebrew
 
-  # Bring default plist under our control - taken from Mountain Lion 10.8.2
-
-  file { '/System/Library/LaunchDaemons/org.apache.httpd.plist':
-    content => template('apache/org.apache.httpd.plist.erb'),
-    group   => 'wheel',
-    notify  => Service['org.apache.httpd'],
-    owner   => 'root'
-  }
-
   # Add all the directories and files Apache is expecting
   # Most of these should already exist on Mountain Lion
-
   file { [
     $apache::config::configdir,
     $apache::config::logdir,

99d3dd491cf588198caf021dbb90b1bb8535aaa2
Florian Weber <florian.weber@ueberbit.de>
2016-11-17 21:52:02 +0100

    Install dev.httpd, disable (and remove) org.apache.httpd and homebrew.mxcl.httpd24

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,13 @@ class apache {
   require apache::config
   require homebrew
 
+  file { '/Library/LaunchDaemons/dev.httpd.plist':
+    content => template('apache/dev.httpd.plist.erb'),
+    group   => 'wheel',
+    notify  => Service['dev.httpd'],
+    owner   => 'root'
+  }
+
   # Add all the directories and files Apache is expecting
   # Most of these should already exist on Mountain Lion
   file { [
@@ -16,11 +23,24 @@ class apache {
 
   file { $apache::config::configfile:
     content => template('apache/config/apache/httpd.conf.erb'),
-    notify  => Service['org.apache.httpd']
+    notify  => Service['dev.httpd']
+  }
+
+  service { "dev.httpd":
+    ensure  => running,
+    enable => true,
+    require => Package['homebrew/apache/httpd24'],
   }
 
   service { "org.apache.httpd":
-    ensure => stopped
+    before => Service['dev.httpd'],
+    ensure => stopped,
+  }
+
+  service { "homebrew.mxcl.httpd24":
+    before => Service['dev.httpd'],
+    ensure => stopped,
+    enable => false,
   }
 
 }
